4f53b10df979de580273d65d12e288390933a645
Phil <m.philipkang@gmail.com>
2021-08-19 12:06:56 -0400

    electron basics

---
+++ main.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+// SET ENV
+process.env.NODE_ENV = 'development';
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+// listen for app to be ready
+app.on('ready', function () {
+  // Create new window
+  mainWindow = new BrowserWindow({});
+  // Load HTML file into the window
+  console.log(__dirname)
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, '/client/index.html'),
+    protocol: 'file',
+    slashes: true,
+  })); 
+})
