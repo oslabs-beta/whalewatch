f53c1d09d92d9c42f08171f6a8eed164a9a49338
matilda wang <matildawangelliman@gmail.com>
2021-08-19 18:46:42 -0400

    changed

---
+++ client/components/authentication/SignUp/SignUpForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignUpForm = () => {
+    return (
+        <div>
+            <h1>Welcome! Creat Your Account</h1>
+            
+        </div>
+    )
+}
+
+export default SignUpForm
---
+++ client/components/authentication/SignUp/form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from 'SignUpform'
+
+const form = () => {
+    return (
+        <div>
+            <SignUpform/>
+        </div>
+    )
+}
+
+export default form
