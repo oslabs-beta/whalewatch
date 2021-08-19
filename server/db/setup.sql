73c5b3a828fe511c159307b0f553d6a178d5c3e0
rjpatt <racheljpatterson@gmail.com>
2021-08-19 11:22:12 -0400

    added SQL setup and db connect files

---
+++ server/db/setup.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username varchar(50) UNIQUE NOT NULL,
+  email varchar(100) UNIQUE NOT NULL,
+  password varchar(100) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS containers (
+  id SERIAL PRIMARY KEY,
+  dockerId integer NOT NULL,
+  name varchar(100) NOT NULL,
+  size integer,
+  status varchar(50),
+  owner integer REFERENCES users(id) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS stats (
+  id SERIAL PRIMARY KEY,
+  container integer REFERENCES containers(id) NOT NULL,
+  timestamp date NOT NULL,
+  cpuUsage integer NOT NULL,
+  memUsage integer NOT NULL,
+  netIo varchar(50) NOT NULL
+  blockIo varchar(50) NOT NULL,
+  pids integer NOT NULL,
+  reqPerMin integer
+)
