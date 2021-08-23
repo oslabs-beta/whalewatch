b236254764896404c560a88d09772b69e120688e
Rachel Patterson <racheljpatterson@gmail.com>
2021-08-23 18:47:07 -0400

    tiny changes

--- client/App.jsx
+++ client/App.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/authentication/Login';
 import DashboardContainer from './containers/DashboardContainer';
-
 import ContainersContainer from './containers/ContainersContainer';
 import NotificationsContainer from './containers/NotificationsContainer';
-
-
 // import navBar from './components/afterLogin/navBar'
 // import listOfContainers from './components/afterLogin/listOfContainers';
 // import dashBoard from './components/afterLogin/dashBoard';
@@ -17,13 +14,12 @@ import './styles.scss';
 const App = () => {
   return (
     <>
-
-
       <Router>
         <h1>WhaleWatch</h1>
         <Switch>
           <Route exact path="/dashboard" component={DashboardContainer} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Form} />
           <Route exact path="/containers" component={ContainersContainer} />
           <Route path="/notification" component={NotificationsContainer} />
         </Switch>
@@ -31,7 +27,5 @@ const App = () => {
     </>
   );
 };
-
-
 
 export default App;
