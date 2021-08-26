56d38ab0beed353eee3776a7bcb944c2d3bbae6e
Phil <m.philipkang@gmail.com>
2021-08-26 10:19:29 -0400

    Phil settings additions

--- client/containers/SettingsContainer.jsx
+++ client/containers/SettingsContainer.jsx
@@ -9,65 +9,65 @@ import colors from '../../assets/theme/colors';
 import AppModal from '../common/AppModal';
 import Icon from '../common/Icon';
 
-const SettingsContainer = ({
-  modalVisible,
-  setModalVisible,
-  settingsOptions,
-  prefArr,
-}) => {
-  return (
-    <>
-      <AppModal
-        modalVisible={modalVisible}
-        modalFooter={<></>}
-        closeOnTouchOutside={false}
-        modalBody={
-          <View>
-            {prefArr.map(({name, selected, onPress}) => (
-              <View key={name}>
-                <TouchableOpacity
-                  onPress={onPress}
-                  style={{
-                    flexDirection: 'row',
-                    paddingVertical: 5,
-                    alignItems: 'center',
-                  }}>
-                  {selected && <Icon size={17} name="check" type="material" />}
-                  <Text style={{fontSize: 17, paddingLeft: selected ? 15 : 30}}>
-                    {name}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            ))}
-          </View>
-        }
-        title="Sort by"
-        setModalVisible={setModalVisible}
-      />
-      <ScrollView style={{backgroundColor: colors.white}}>
-        {settingsOptions.map(({title, subTitle, onPress}, index) => (
-          <TouchableOpacity key={title} onPress={onPress}>
-            <View
-              style={{
-                paddingHorizontal: 20,
-                paddingBottom: 20,
-                paddingTop: 20,
-              }}>
-              <Text style={{fontSize: 17}}>{title}</Text>
-              {subTitle && (
-                <Text style={{fontSize: 14, opacity: 0.5, paddingTop: 5}}>
-                  {subTitle}
-                </Text>
-              )}
-            </View>
+// const SettingsContainer = ({
+//   modalVisible,
+//   setModalVisible,
+//   settingsOptions,
+//   prefArr,
+// }) => {
+//   return (
+//     <>
+//       <AppModal
+//         modalVisible={modalVisible}
+//         modalFooter={<></>}
+//         closeOnTouchOutside={false}
+//         modalBody={
+//           <View>
+//             {prefArr.map(({name, selected, onPress}) => (
+//               <View key={name}>
+//                 <TouchableOpacity
+//                   onPress={onPress}
+//                   style={{
+//                     flexDirection: 'row',
+//                     paddingVertical: 5,
+//                     alignItems: 'center',
+//                   }}>
+//                   {selected && <Icon size={17} name="check" type="material" />}
+//                   <Text style={{fontSize: 17, paddingLeft: selected ? 15 : 30}}>
+//                     {name}
+//                   </Text>
+//                 </TouchableOpacity>
+//               </View>
+//             ))}
+//           </View>
+//         }
+//         title="Sort by"
+//         setModalVisible={setModalVisible}
+//       />
+//       <ScrollView style={{backgroundColor: colors.white}}>
+//         {settingsOptions.map(({title, subTitle, onPress}, index) => (
+//           <TouchableOpacity key={title} onPress={onPress}>
+//             <View
+//               style={{
+//                 paddingHorizontal: 20,
+//                 paddingBottom: 20,
+//                 paddingTop: 20,
+//               }}>
+//               <Text style={{fontSize: 17}}>{title}</Text>
+//               {subTitle && (
+//                 <Text style={{fontSize: 14, opacity: 0.5, paddingTop: 5}}>
+//                   {subTitle}
+//                 </Text>
+//               )}
+//             </View>
 
-            <View style={{height: 0.5, backgroundColor: colors.grey}} />
-          </TouchableOpacity>
-        ))}
-      </ScrollView>
-    </>
-  );
-};
+//             <View style={{height: 0.5, backgroundColor: colors.grey}} />
+//           </TouchableOpacity>
+//         ))}
+//       </ScrollView>
+//     </>
+//   );
+// };
 
 
 // const SettingsContainer = (props) => {
