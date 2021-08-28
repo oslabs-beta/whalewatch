c5e7e779af9131bd61818bf22f277d94ad0ca2be
Rachel Patterson <racheljpatterson@gmail.com>
2021-08-28 12:17:12 -0400

    further changes to docker cli helpers

--- server/helpers/dockerCliHelper.js
+++ server/helpers/dockerCliHelper.js
@@ -5,7 +5,15 @@ dockerApiHelper.getContainerList = () => {
   exec(
     'docker ps --all --format "{{json .}}," --size',
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return [...stdout];
     }
   )
 
@@ -15,7 +23,15 @@ dockerApiHelper.inspectContainer = (id) => {
   exec(
     `docker inspect --format "{{json .}}," ${id}`,
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return [...stdout];
     }
   )
 }
@@ -24,7 +40,15 @@ dockerApiHelper.getStats = () => {
   exec(
     'docker stats --no-stream --format "{{json .}},"',
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return [...stdout];
     }
   )
 }
@@ -33,7 +57,15 @@ dockerApiHelper.startContainer = (id) => {
   exec(
     `docker start ${id}`,
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return;
     }
   )
 }
@@ -42,7 +74,15 @@ dockerApiHelper.stopContainer = (id) => {
   exec(
     `docker stop ${id}`,
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return;
     }
   )
 }
@@ -51,7 +91,15 @@ dockerApiHelper.restartContainer = (id) => {
   exec(
     `docker rm ${id}`,
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return;
     }
   )
 }
@@ -60,7 +108,15 @@ dockerApiHelper.removeContainer = (id) => {
   exec(
     `docker start ${id}`,
     (error, stdout, stderr) => {
-
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      return;
     }
   )
 }
