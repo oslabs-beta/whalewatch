1419e7d4593d0119c87e1cb24eafffd073611f19
Rachel Patterson <racheljpatterson@gmail.com>
2021-08-30 11:10:54 -0400

    more updates

--- server/helpers/dbHelper.js
+++ server/helpers/dbHelper.js
@@ -1,4 +1,9 @@
 const dockerApiHelper = require('../helpers/dockerApiHelper');
 const dbHelper = {};
 
+dbHelper.refreshContainerData = async () => {
+  const containers = await dockerApiHelper.getContainerList;
+  const insertQuery = 'INSERT INTO containers ('
+}
+
 module.exports = dbHelper;
