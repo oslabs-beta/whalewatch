e3b83eb62efd54419f6f534ab035b49da4ec1f81
Rachel Patterson <racheljpatterson@gmail.com>
2021-09-09 13:39:26 -0400

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const PORT = 3000;
 const schema = require('./db/schema.js');
 const expressJwt = require('express-jwt');
-const {ApolloServer,gql} = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
@@ -13,12 +13,13 @@ app.use(express.json());
 app.use(express.static('build'));
 app.use(cookieParser());
 //use our graphql middleware. only to one endpoint
-app.use('/graphql', (req,res) => {
+app.use('/graphql', (req, res) => {
   return graphqlHTTP({
-  schema,
-  context: {req,res},
-  graphiql:true
-})(req,res)}
+    schema,
+    context: { req, res },
+    graphiql: true
+  })(req, res)
+}
 )
 
 //allow access to our index.html folder
@@ -28,7 +29,7 @@ app.use('/', express.static(path.join(__dirname, '../client')));
 if (process.env.NODE_ENV === 'production') {
   //allow access to our index.html folder
   app.use('/build', express.static(path.join(__dirname, '../build')));
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
   });
 }
